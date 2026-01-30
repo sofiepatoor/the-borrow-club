9c6cc1e88e5193e26438046dfa16b885449ff6c1
sofiepatoor <sofie.patoor@gmail.com>
2026-01-31 00:04:36 +0100

    chore: move styles to directory outside app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './global-styles/global.scss';
+import '@/styles/global.scss';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
