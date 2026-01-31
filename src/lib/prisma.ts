3159385d67d641d5a3a90463614de29f29eab635
sofiepatoor <sofie.patoor@gmail.com>
2026-01-31 15:26:03 +0100

    chore: reuse Prisma Client instance if one exists for better ORM performance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,9 +2,12 @@ import 'dotenv/config';
 import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
 
+// reuse Prisma Client instance if one exists
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
 const connectionString = `${process.env.DATABASE_URL}`;
-
 const adapter = new PrismaNeon({ connectionString });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
