3a1c82c00c96d398002103d9d87b90460779479c
sofiepatoor <sofie.patoor@gmail.com>
2026-02-12 21:59:52 +0100

    add: rubik as main font-family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,13 @@
 import type { Metadata } from 'next';
-import { Neuton, Montserrat } from 'next/font/google';
+import { Rubik } from 'next/font/google';
 import '@/styles/global.scss';
 import Header from '@/components/ui/Header';
 import Footer from '@/components/ui/Footer';
 
-const neuton = Neuton({
-  variable: '--font-neuton',
+const rubik = Rubik({
+  variable: '--font-rubik',
   subsets: ['latin'],
-  weight: ['400', '700'],
-});
-
-const montserrat = Montserrat({
-  variable: '--font-montserrat',
-  subsets: ['latin'],
-  weight: ['400', '700'],
+  weight: ['400', '600', '700'],
 });
 
 export const metadata: Metadata = {
@@ -27,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${neuton.variable} ${montserrat.variable}`}>
+    <html lang="en" className={`${rubik.variable}`}>
+      <body>
         <Header />
         <main>{children}</main>
         <Footer />
