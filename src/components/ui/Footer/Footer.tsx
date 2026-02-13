aaa7426aaa522d646899c953f9c91cca3db03cc4
sofiepatoor <sofie.patoor@gmail.com>
2026-02-13 23:46:33 +0100

    add: basic footer layout

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,8 +1,24 @@
+import Container from '../Container';
 import styles from './footer.module.scss';
 
 function Footer() {
   return (
-    <footer className={styles.wrapper}>{/* TODO: Add footer content */}</footer>
+    <footer className={styles.footer}>
+      <Container>
+        <div className={styles.footerContent}>
+          <p>
+            <a
+              href="https://www.sofiepatoor.be"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.footerLink}
+            >
+              Made by Sofie
+            </a>
+          </p>
+        </div>
+      </Container>
+    </footer>
   );
 }
 
