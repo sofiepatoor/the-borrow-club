c31c35502bb9041015ec56a5f2b6b3439cfac322
sofiepatoor <sofie.patoor@gmail.com>
2026-02-19 20:57:36 +0100

    refactor: button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,15 +1,80 @@
-import React from 'react';
+import clsx from 'clsx';
+import Link from 'next/link';
+import {
+  forwardRef,
+  type ButtonHTMLAttributes,
+  type AnchorHTMLAttributes,
+  type ReactNode,
+} from 'react';
+
+type BaseProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+type ButtonAsButton = BaseProps &
+  ButtonHTMLAttributes<HTMLButtonElement> & {
+    href?: undefined;
+  };
+
+type ButtonAsLink = BaseProps &
+  AnchorHTMLAttributes<HTMLAnchorElement> & {
+    href: string;
+  };
+
+type Props = ButtonAsButton | ButtonAsLink;
+
 import styles from './button.module.scss';
 
-function Button({
-  children,
-  ...delegated
-}: React.ComponentPropsWithoutRef<'button'>) {
-  return (
-    <button className={styles.button} {...delegated}>
-      {children}
-    </button>
-  );
-}
+const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, Props>(
+  function Button(props, ref) {
+    const { children, className } = props;
+    const classNames = clsx(styles.button, className);
+
+    if ('href' in props && props.href) {
+      const { href, ...rest } = props;
+      const isExternal = href.startsWith('http') || href.startsWith('//');
+
+      if (isExternal) {
+        return (
+          <a
+            ref={ref as React.Ref<HTMLAnchorElement>}
+            href={href}
+            className={classNames}
+            target="_blank"
+            rel="noopener noreferrer"
+            {...rest}
+          >
+            {children}
+          </a>
+        );
+      }
+
+      return (
+        <Link
+          ref={ref as React.Ref<HTMLAnchorElement>}
+          href={href}
+          className={classNames}
+          {...rest}
+        >
+          {children}
+        </Link>
+      );
+    }
+
+    const buttonProps = props as ButtonAsButton;
+    const { type, ...rest } = buttonProps;
+    return (
+      <button
+        ref={ref as React.Ref<HTMLButtonElement>}
+        type={type ?? 'button'}
+        {...rest}
+        className={classNames}
+      >
+        {children}
+      </button>
+    );
+  },
+);
 
 export default Button;
